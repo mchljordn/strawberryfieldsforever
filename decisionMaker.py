4feabb2ad6ecbf1b37343540594a4183c09fe735
mchljordn <si.cupank05@gmail.com>
2025-01-29 16:30:43 +0700

    add decisionMaker.py

--- decisionMaker.py
+++ decisionMaker.py
@@ -1,17 +1,39 @@
 import random
 import time
+import colorama
 import keyboard
 
+colorama.init(autoreset=True)
+
 def on_space_pressed():
-    print("Spacebar pressed. Tossing the coin...")
-    time.sleep(1)
+    time.sleep(0.2)
+    print(colorama.Fore.YELLOW + "Spacebar pressed.", end='')
+    time.sleep(0.3)
+    print(colorama.Fore.GREEN + " Tossing the coin", end="")
+    for _ in range(3):
+        time.sleep(0.2)
+        print(".", end="", flush=True)
+    print()
+    time.sleep(0.45)
     random_bool = random.choice([True, False])
     if random_bool:
-        print("Head")
+        print(colorama.Fore.RED + "Head")
     else:
-        print("Tail")
+        print(colorama.Fore.BLUE + "Tail")
 
 if __name__ == "__main__":
     keyboard.add_hotkey('space', on_space_pressed)
-    print("Press spacebar to toss the coin. Press 'q' to quit.")
-    keyboard.wait('q')
+    print("Press spacebar to toss the coin. Press 'q' or 'ctrl + c' to quit.")
+    try:
+        keyboard.wait('q')
+        print("Quitting", end="")
+        for _ in range(3):
+            time.sleep(0.5)
+            print(".", end="", flush=True)
+        print()
+    except KeyboardInterrupt as e:
+        print("Quitting", end="")
+        for _ in range(3):
+            time.sleep(0.5)
+            print(".", end="", flush=True)
+        print()
